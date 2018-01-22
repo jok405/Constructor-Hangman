dc5de60087ac1a0e02086137ec597d73ab564476
jok405 <john.y.kim84@gmail.com>
2018-01-21 22:54:37 -0500

    initial constructor containing array of words

---
+++ gamewords.js
@@ -0,0 +1,9 @@
+// ==============  blackJack.js | Composed by John Kim | Univeristy of Richmond  ============================= 
+
+// ==============  | A tribute to Megaman 3 hangman game | ============================
+
+// ============== | A word list constructor | ====================================
+
+var gameWords = ['Magnet Man', 'Hard Man', 'Top Man', 'Shadow Man', 'Spark Man', 'Snake Man', 'Gemini Man', 'Needle Man'];
+
+module.exports = gameWords;
